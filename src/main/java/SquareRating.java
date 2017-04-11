7ecc52b9bbec8063ed416818e7f223b14f2d4f0e
jonesal26gc <holly001git>
2017-04-11 14:46:13 +0100

    Alter the square ratings

--- src/main/java/SquareRating.java
+++ src/main/java/SquareRating.java
@@ -3,14 +3,14 @@ import java.util.List;
 import static java.util.Arrays.asList;
 
 public enum SquareRating {
-    ROW_7(7,asList(9,0,0,0,0,0,0,9)),
-    ROW_6(6,asList(0,0,0,0,0,0,0,0)),
-    ROW_5(5,asList(0,0,9,9,9,9,0,0)),
-    ROW_4(4,asList(0,0,9,0,0,9,0,0)),
-    ROW_3(3,asList(0,0,9,0,0,9,0,0)),
-    ROW_2(2,asList(0,0,9,9,9,9,0,0)),
-    ROW_1(1,asList(0,0,0,0,0,0,0,0)),
-    ROW_0(0,asList(9,0,0,0,0,0,0,9));
+    ROW_7(7,asList(9,4,6,6,6,6,4,9)),
+    ROW_6(6,asList(4,0,0,0,0,0,0,4)),
+    ROW_5(5,asList(6,0,9,7,7,9,0,6)),
+    ROW_4(4,asList(6,0,7,0,0,7,0,6)),
+    ROW_3(3,asList(6,0,7,0,0,7,0,6)),
+    ROW_2(2,asList(6,0,9,7,7,9,0,6)),
+    ROW_1(1,asList(4,0,0,0,0,0,0,4)),
+    ROW_0(0,asList(9,4,6,6,6,6,4,9));
 
     private final int yAxis;
     private final List<Integer> preferenceRating;
